272e2213df54a245f9441df038a75276171f24e8
Carl Li <leekunhui@sina.cn>
2021-04-06 22:31:45 +1200

    Add project files.

---
+++ SkinetInfrastructure/Data/StoreContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkinetCore.Entities;
+
+namespace SkinetInfrastructure.Data
+{
+    public class StoreContext: DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options): base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
